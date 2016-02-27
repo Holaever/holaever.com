27062972327262aeddc49164b11eace7146d39e6
Tony Stark <swzyocowboy@icloud.com>
2016-02-27 12:57:08 +0800

    更新了首页内容：初始化主要布局、页尾

--- dev/script/page/index.js
+++ dev/script/page/index.js
@@ -1,9 +1,64 @@
+'use strict';
+
+
+
 (function () {
     var page;
     page = document.createElement('div');
     page.id = 'page';
     document.body.insertBefore(page, document.body.firstChild);
 })()
+
+
+
+
+var content = {
+    id: 0,
+    members:[{
+        id: 0.1,
+        'photo': 'public/img/st/team-1.jpg',
+        'name': 'Robert T. Williams',
+        'function': 'Graphic Designer',
+        'desc': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.',
+        'social': '@robertwilliams'
+    }, {
+        id: 0.2,
+        'photo': 'public/img/st/team-1.jpg',
+        'name': 'Robert T. Williams',
+        'function': 'Graphic Designer',
+        'desc': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.',
+        'social': '@robertwilliams'
+    }, {
+        id: 0.3,
+        'photo': 'public/img/st/team-1.jpg',
+        'name': 'Robert T. Williams',
+        'function': 'Graphic Designer',
+        'desc': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.',
+        'social': '@robertwilliams'
+    }],
+    about: [{
+        id: 0.1,
+        title: 'About Us',
+        disc: [
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. '
+        ]
+    }, {
+        id: 0.1,
+        title: 'Our Mission',
+        disc: [
+            'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. <br>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+        ]
+    }, {
+        id: 0.1,
+        title: 'Our Clients',
+        disc: [
+            'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. ',
+            'Nemo enim ipsam voluptatem quia voluptas'
+        ]
+    }]
+};
+
+
 
 class PageHeader extends React.Component {
     constructor(props) {
@@ -60,26 +115,181 @@ class PageHeader extends React.Component {
 
 
 
-// class Portrait extends React.Component {
-//     constructor(props) {
-//         super(props);
-//     }
-//     render() {
-//         return (
-//             <div className="rk-main">
-//                 <div className="">
-//                     <div className="">
-//                         <img src="public/img/st/team-1.jpg" alt="" className=""/>
-//                         <h5 className="">Robert T. Williams</h5>
-//                         <p className="">Graphic Designer</p>
-//                         <p className="">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
-//                         <a href="#" className="">@robertwilliams</a>
-//                     </div>
-//                 </div>
-//             </div>
-//         );
-//     }
-// }
+
+class Portrait extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <div className="ae-grid__item item-lg-4 ae-kappa au-mb-3">
+                <img src={this.props.member.photo} alt="" className="au-mb-3"/>
+                <h5 className="ae-u-bolder au-mt-2">{this.props.member.name}</h5>
+                <p className="ae-u-bolder au-mb-3">{this.props.member.function}</p>
+                <p className="au-lg-ta-left au-mb-3 au-pl-4 au-pr-4">{this.props.member.desc}</p>
+                <a href="#" className="ae-u-bolder au-underline">{this.props.member.social}</a>
+            </div>
+        );
+    }
+}
+
+class Portraits extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <div className="ae-grid au-xs-ta-center au-mb-4">
+                {
+                    this.props.members.map((member) => {
+                        return <Portrait key={member.id} member={member}/>;
+                    })
+                }
+            </div>
+        );
+    }
+}
+
+
+
+class Quotes extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <div className="ae-grid au-pt-2">
+                <div className="ae-grid__item item-lg-8 item-lg--offset-2">
+                    <h4 className="ae-u-bolder">Title example</h4>
+                    <p>
+                        Totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur.
+                    </p>
+                </div>
+            </div>
+        );
+    }
+}
+
+
+
+class About extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        var theAbout = this.props.about.map((abt) => {
+            return (
+                <div className="ae-grid__item--alt item-lg-4">
+                    <h6 className="ae-u-boldest">{abt.title}</h6>
+                    {
+                        abt.disc.map((element) => {
+                            return (
+                                <p className="au-lh-3">
+                                    {element}
+                                </p>
+                            )
+                        })
+                    }
+                </div>
+            );
+        });
+        return (
+            <div className="ae-grid ae-grid--alt au-xs-ptp-1">
+                {theAbout}
+            </div>
+        );
+    }
+}
+
+
+
+class Main extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <div className="ae-container-fluid ae-container-fluid--inner rk-main">
+                <Portraits members={content.members}/>
+                <Quotes/>
+                <About about={content.about}/>
+            </div>
+        );
+    }
+}
+
+
+
+class Footer extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        var facebook = '<use xlink:href="public/img/st/symbols.svg#icon-facebook"></use>',
+            twitter = '<use xlink:href="public/img/st/symbols.svg#icon-twitter"></use>',
+            pinterest = '<use xlink:href="public/img/st/symbols.svg#icon-pinterest"></use>',
+            tumblr = '<use xlink:href="public/img/st/symbols.svg#icon-tumblr"></use>';
+        return (
+            <div className="ae-container-fluid rk-footer">
+                <div className="ae-grid ae-grid--collapse">
+                    <div className="ae-grid__item item-lg-4 au-xs-ta-center au-lg-ta-left">
+                        <ul className="rk-menu rk-footer-menu">
+                            <li className="rk-menu__item">
+                                <a href="about.html" className="rk-menu__link">关于</a>
+                            </li>
+                            <li className="rk-menu__item">
+                                <a href="documentation.html" className="rk-menu__link">作品</a>
+                            </li>
+                            <li className="rk-menu__item">
+                                <a href="contact.html" className="rk-menu__link">联系</a>
+                            </li>
+                        </ul>
+                        <p className="rk-footer__text rk-footer__copy ">
+                            <span className="ae-u-bold">© </span>
+                            <span className="ae-u-bolder">2016 Holaever, Inc </span>
+                            保留一切权力。
+                        </p>
+                    </div>
+
+                    <div className="ae-grid__item item-lg-4 au-xs-ta-center">
+                        <a href="#0" className="rk-social-btn">
+                            <svg>
+                                <svg dangerouslySetInnerHTML={{__html: facebook}}></svg>
+                            </svg>
+                        </a>
+                        <a href="#0" className="rk-social-btn">
+                            <svg>
+                                <svg dangerouslySetInnerHTML={{__html: twitter}}></svg>
+                            </svg>
+                        </a>
+                        <a href="#0" className="rk-social-btn">
+                            <svg>
+                                <svg dangerouslySetInnerHTML={{__html: pinterest}}></svg>
+                            </svg>
+                        </a>
+                        <a href="#0" className="rk-social-btn">
+                            <svg>
+                                <svg dangerouslySetInnerHTML={{__html: tumblr}}></svg>
+                            </svg>
+                        </a>
+                    </div>
+
+                    <div className="ae-grid__item item-lg-4 au-xs-ta-center au-lg-ta-right">
+                        <p className="rk-footer__text rk-footer__contact ">
+                            <span className="ae-u-bold">电子邮件: </span>
+                            <span className="ae-u-bolder">
+                                <a href="mailto:hello@holaever.com" className="rk-dark-color ">hello@holaever.com</a>
+                            </span>
+                        </p>
+                        <p class="rk-footer__text rk-footer__by">
+                            我们打造非比寻常、电影级别的软件产品!
+                        </p>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
 
 
 
@@ -91,7 +301,8 @@ class Page extends React.Component {
         return (
             <div>
                 <PageHeader/>
-                {/*<Portrait/>*/}
+                <Main/>
+                <Footer/>
             </div>
         );
     }
--- public/js/page/index.js
+++ public/js/page/index.js
@@ -15,6 +15,45 @@ function _inherits(subClass, superClass) { if (typeof superClass !== "function" 
     document.body.insertBefore(page, document.body.firstChild);
 })();
 
+var content = {
+    id: 0,
+    members: [{
+        id: 0.1,
+        'photo': 'public/img/st/team-1.jpg',
+        'name': 'Robert T. Williams',
+        'function': 'Graphic Designer',
+        'desc': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.',
+        'social': '@robertwilliams'
+    }, {
+        id: 0.2,
+        'photo': 'public/img/st/team-1.jpg',
+        'name': 'Robert T. Williams',
+        'function': 'Graphic Designer',
+        'desc': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.',
+        'social': '@robertwilliams'
+    }, {
+        id: 0.3,
+        'photo': 'public/img/st/team-1.jpg',
+        'name': 'Robert T. Williams',
+        'function': 'Graphic Designer',
+        'desc': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.',
+        'social': '@robertwilliams'
+    }],
+    about: [{
+        id: 0.1,
+        title: 'About Us',
+        disc: ['Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ']
+    }, {
+        id: 0.1,
+        title: 'Our Mission',
+        disc: ['Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. <br>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.']
+    }, {
+        id: 0.1,
+        title: 'Our Clients',
+        disc: ['Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. ', 'Nemo enim ipsam voluptatem quia voluptas']
+    }]
+};
+
 var PageHeader = function (_React$Component) {
     _inherits(PageHeader, _React$Component);
 
@@ -130,29 +169,325 @@ var PageHeader = function (_React$Component) {
     return PageHeader;
 }(React.Component);
 
-// class Portrait extends React.Component {
-//     constructor(props) {
-//         super(props);
-//     }
-//     render() {
-//         return (
-//             <div className="rk-main">
-//                 <div className="">
-//                     <div className="">
-//                         <img src="public/img/st/team-1.jpg" alt="" className=""/>
-//                         <h5 className="">Robert T. Williams</h5>
-//                         <p className="">Graphic Designer</p>
-//                         <p className="">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
-//                         <a href="#" className="">@robertwilliams</a>
-//                     </div>
-//                 </div>
-//             </div>
-//         );
-//     }
-// }
+var Portrait = function (_React$Component2) {
+    _inherits(Portrait, _React$Component2);
 
-var Page = function (_React$Component2) {
-    _inherits(Page, _React$Component2);
+    function Portrait(props) {
+        _classCallCheck(this, Portrait);
+
+        return _possibleConstructorReturn(this, Object.getPrototypeOf(Portrait).call(this, props));
+    }
+
+    _createClass(Portrait, [{
+        key: 'render',
+        value: function render() {
+            return React.createElement(
+                'div',
+                { className: 'ae-grid__item item-lg-4 ae-kappa au-mb-3' },
+                React.createElement('img', { src: this.props.member.photo, alt: '', className: 'au-mb-3' }),
+                React.createElement(
+                    'h5',
+                    { className: 'ae-u-bolder au-mt-2' },
+                    this.props.member.name
+                ),
+                React.createElement(
+                    'p',
+                    { className: 'ae-u-bolder au-mb-3' },
+                    this.props.member.function
+                ),
+                React.createElement(
+                    'p',
+                    { className: 'au-lg-ta-left au-mb-3 au-pl-4 au-pr-4' },
+                    this.props.member.desc
+                ),
+                React.createElement(
+                    'a',
+                    { href: '#', className: 'ae-u-bolder au-underline' },
+                    this.props.member.social
+                )
+            );
+        }
+    }]);
+
+    return Portrait;
+}(React.Component);
+
+var Portraits = function (_React$Component3) {
+    _inherits(Portraits, _React$Component3);
+
+    function Portraits(props) {
+        _classCallCheck(this, Portraits);
+
+        return _possibleConstructorReturn(this, Object.getPrototypeOf(Portraits).call(this, props));
+    }
+
+    _createClass(Portraits, [{
+        key: 'render',
+        value: function render() {
+            return React.createElement(
+                'div',
+                { className: 'ae-grid au-xs-ta-center au-mb-4' },
+                this.props.members.map(function (member) {
+                    return React.createElement(Portrait, { key: member.id, member: member });
+                })
+            );
+        }
+    }]);
+
+    return Portraits;
+}(React.Component);
+
+var Quotes = function (_React$Component4) {
+    _inherits(Quotes, _React$Component4);
+
+    function Quotes(props) {
+        _classCallCheck(this, Quotes);
+
+        return _possibleConstructorReturn(this, Object.getPrototypeOf(Quotes).call(this, props));
+    }
+
+    _createClass(Quotes, [{
+        key: 'render',
+        value: function render() {
+            return React.createElement(
+                'div',
+                { className: 'ae-grid au-pt-2' },
+                React.createElement(
+                    'div',
+                    { className: 'ae-grid__item item-lg-8 item-lg--offset-2' },
+                    React.createElement(
+                        'h4',
+                        { className: 'ae-u-bolder' },
+                        'Title example'
+                    ),
+                    React.createElement(
+                        'p',
+                        null,
+                        'Totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur.'
+                    )
+                )
+            );
+        }
+    }]);
+
+    return Quotes;
+}(React.Component);
+
+var About = function (_React$Component5) {
+    _inherits(About, _React$Component5);
+
+    function About(props) {
+        _classCallCheck(this, About);
+
+        return _possibleConstructorReturn(this, Object.getPrototypeOf(About).call(this, props));
+    }
+
+    _createClass(About, [{
+        key: 'render',
+        value: function render() {
+            var theAbout = this.props.about.map(function (abt) {
+                return React.createElement(
+                    'div',
+                    { className: 'ae-grid__item--alt item-lg-4' },
+                    React.createElement(
+                        'h6',
+                        { className: 'ae-u-boldest' },
+                        abt.title
+                    ),
+                    abt.disc.map(function (element) {
+                        return React.createElement(
+                            'p',
+                            { className: 'au-lh-3' },
+                            element
+                        );
+                    })
+                );
+            });
+            return React.createElement(
+                'div',
+                { className: 'ae-grid ae-grid--alt au-xs-ptp-1' },
+                theAbout
+            );
+        }
+    }]);
+
+    return About;
+}(React.Component);
+
+var Main = function (_React$Component6) {
+    _inherits(Main, _React$Component6);
+
+    function Main(props) {
+        _classCallCheck(this, Main);
+
+        return _possibleConstructorReturn(this, Object.getPrototypeOf(Main).call(this, props));
+    }
+
+    _createClass(Main, [{
+        key: 'render',
+        value: function render() {
+            return React.createElement(
+                'div',
+                { className: 'ae-container-fluid ae-container-fluid--inner rk-main' },
+                React.createElement(Portraits, { members: content.members }),
+                React.createElement(Quotes, null),
+                React.createElement(About, { about: content.about })
+            );
+        }
+    }]);
+
+    return Main;
+}(React.Component);
+
+var Footer = function (_React$Component7) {
+    _inherits(Footer, _React$Component7);
+
+    function Footer(props) {
+        _classCallCheck(this, Footer);
+
+        return _possibleConstructorReturn(this, Object.getPrototypeOf(Footer).call(this, props));
+    }
+
+    _createClass(Footer, [{
+        key: 'render',
+        value: function render() {
+            var facebook = '<use xlink:href="public/img/st/symbols.svg#icon-facebook"></use>',
+                twitter = '<use xlink:href="public/img/st/symbols.svg#icon-twitter"></use>',
+                pinterest = '<use xlink:href="public/img/st/symbols.svg#icon-pinterest"></use>',
+                tumblr = '<use xlink:href="public/img/st/symbols.svg#icon-tumblr"></use>';
+            return React.createElement(
+                'div',
+                { className: 'ae-container-fluid rk-footer' },
+                React.createElement(
+                    'div',
+                    { className: 'ae-grid ae-grid--collapse' },
+                    React.createElement(
+                        'div',
+                        { className: 'ae-grid__item item-lg-4 au-xs-ta-center au-lg-ta-left' },
+                        React.createElement(
+                            'ul',
+                            { className: 'rk-menu rk-footer-menu' },
+                            React.createElement(
+                                'li',
+                                { className: 'rk-menu__item' },
+                                React.createElement(
+                                    'a',
+                                    { href: 'about.html', className: 'rk-menu__link' },
+                                    '关于'
+                                )
+                            ),
+                            React.createElement(
+                                'li',
+                                { className: 'rk-menu__item' },
+                                React.createElement(
+                                    'a',
+                                    { href: 'documentation.html', className: 'rk-menu__link' },
+                                    '作品'
+                                )
+                            ),
+                            React.createElement(
+                                'li',
+                                { className: 'rk-menu__item' },
+                                React.createElement(
+                                    'a',
+                                    { href: 'contact.html', className: 'rk-menu__link' },
+                                    '联系'
+                                )
+                            )
+                        ),
+                        React.createElement(
+                            'p',
+                            { className: 'rk-footer__text rk-footer__copy ' },
+                            React.createElement(
+                                'span',
+                                { className: 'ae-u-bold' },
+                                '© '
+                            ),
+                            React.createElement(
+                                'span',
+                                { className: 'ae-u-bolder' },
+                                '2016 Holaever, Inc '
+                            ),
+                            '保留一切权力。'
+                        )
+                    ),
+                    React.createElement(
+                        'div',
+                        { className: 'ae-grid__item item-lg-4 au-xs-ta-center' },
+                        React.createElement(
+                            'a',
+                            { href: '#0', className: 'rk-social-btn' },
+                            React.createElement(
+                                'svg',
+                                null,
+                                React.createElement('svg', { dangerouslySetInnerHTML: { __html: facebook } })
+                            )
+                        ),
+                        React.createElement(
+                            'a',
+                            { href: '#0', className: 'rk-social-btn' },
+                            React.createElement(
+                                'svg',
+                                null,
+                                React.createElement('svg', { dangerouslySetInnerHTML: { __html: twitter } })
+                            )
+                        ),
+                        React.createElement(
+                            'a',
+                            { href: '#0', className: 'rk-social-btn' },
+                            React.createElement(
+                                'svg',
+                                null,
+                                React.createElement('svg', { dangerouslySetInnerHTML: { __html: pinterest } })
+                            )
+                        ),
+                        React.createElement(
+                            'a',
+                            { href: '#0', className: 'rk-social-btn' },
+                            React.createElement(
+                                'svg',
+                                null,
+                                React.createElement('svg', { dangerouslySetInnerHTML: { __html: tumblr } })
+                            )
+                        )
+                    ),
+                    React.createElement(
+                        'div',
+                        { className: 'ae-grid__item item-lg-4 au-xs-ta-center au-lg-ta-right' },
+                        React.createElement(
+                            'p',
+                            { className: 'rk-footer__text rk-footer__contact ' },
+                            React.createElement(
+                                'span',
+                                { className: 'ae-u-bold' },
+                                '电子邮件: '
+                            ),
+                            React.createElement(
+                                'span',
+                                { className: 'ae-u-bolder' },
+                                React.createElement(
+                                    'a',
+                                    { href: 'mailto:hello@holaever.com', className: 'rk-dark-color ' },
+                                    'hello@holaever.com'
+                                )
+                            )
+                        ),
+                        React.createElement(
+                            'p',
+                            { 'class': 'rk-footer__text rk-footer__by' },
+                            '我们打造非比寻常、电影级别的软件产品!'
+                        )
+                    )
+                )
+            );
+        }
+    }]);
+
+    return Footer;
+}(React.Component);
+
+var Page = function (_React$Component8) {
+    _inherits(Page, _React$Component8);
 
     function Page(props) {
         _classCallCheck(this, Page);
@@ -166,7 +501,9 @@ var Page = function (_React$Component2) {
             return React.createElement(
                 'div',
                 null,
-                React.createElement(PageHeader, null)
+                React.createElement(PageHeader, null),
+                React.createElement(Main, null),
+                React.createElement(Footer, null)
             );
         }
     }]);
